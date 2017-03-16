79e031d9aacaf50a2fd3759cf2c6e5467bedd8ed
Pol-Valentin Cami <polvalentin@evaneos.com>
2017-03-16 17:09:09 +0100

    feat: send invoice notification before due date

--- spec/invoice_reminder_spec.rb
+++ spec/invoice_reminder_spec.rb
@@ -5,18 +5,21 @@ require_relative '../src/invoice_reminder_service'
 require_relative '../src/invoice_notification_sender_service'
 
 describe 'Service invoice reminder' do
-
-  before(:suite) do
-  end
-
   it "should send a notification 10 days before invoice issue" do
     invoice = Invoice.new('12EDF432', 2300, Date.parse('2017-02-15'), nil,
                           'Doe', 'John', 'john.doe@example.com')
-
-    InvoiceReminderService.process(invoice, Date.parse('2017-02-25'))
     expect(InvoiceNotificationSenderService).to(
-      receive(:send_before_due_date_notification).with(invoice)
+        receive(:send_before_due_date_notification).with(invoice)
     )
+    InvoiceReminderService.process(invoice, Date.parse('2017-02-25'))
   end
 
+  it "should send a dunning_notification one month after due date" do
+    invoice = Invoice.new('12EDF432', 2300, Date.parse('2017-02-15'), nil,
+                          'Doe', 'John', 'john.doe@example.com')
+    expect(InvoiceNotificationSenderService).to(
+        receive(:send_dunning_notification).with(invoice)
+    )
+    InvoiceReminderService.process(invoice, Date.parse('2017-03-25'))
+  end
 end
--- src/invoice_reminder_service.rb
+++ src/invoice_reminder_service.rb
@@ -4,5 +4,8 @@ require './src/invoice_notification_sender_service'
 module InvoiceReminderService
   def self.process(invoice, date = Date.today)
     # do smth with invoice
+    if (date - invoice.due_date).to_i == 10
+      InvoiceNotificationSenderService.send_before_due_date_notification(invoice)
+    end
   end
 end
